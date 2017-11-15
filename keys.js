a1e995e999d42144283190b97757bb19c6c2005c
janetcushing <janet.cushing@gmail.com>
2017-11-15 18:53:44 -0500

    initial files

---
+++ keys.js
@@ -0,0 +1,42 @@
+JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'KruKMtLkNDHVAHZl3obkMkIup',
+  consumer_secret: 'ErDYXPdJx9rzCXPmobCoMHBFP36a0iq0tTbwnMMMSl1iU8KXr2',
+  access_token_key: '930172358081605632-zF7Aw5NBFuQfyt1f69rYYaJvEJP9Si1',
+  access_token_secret: 'FTafTT892CAA8Nw9kLjRme0zofiMnv1G5ODryzKOHzvrY',
+}
+
+module.exports = twitterKeys;
+
+// Consumer Key (API Key)	KruKMtLkNDHVAHZl3obkMkIup
+// Consumer Secret (API Secret)	ErDYXPdJx9rzCXPmobCoMHBFP36a0iq0tTbwnMMMSl1iU8KXr2
+
+// Application Settings
+// Keep the "Consumer Secret" a secret. This key should never be human-readable in your application.
+// Consumer Key (API Key)	KruKMtLkNDHVAHZl3obkMkIup
+// Consumer Secret (API Secret)	ErDYXPdJx9rzCXPmobCoMHBFP36a0iq0tTbwnMMMSl1iU8KXr2
+// Access Level	Read and write (modify app permissions)
+// Owner	cushcushj
+// Owner ID	930172358081605632
+
+// Access Token	930172358081605632-zF7Aw5NBFuQfyt1f69rYYaJvEJP9Si1
+// Access Token Secret	FTafTT892CAA8Nw9kLjRme0zofiMnv1G5ODryzKOHzvrY
+// Access Level	Read and write
+// Owner	cushcushj
+// Owner ID	930172358081605632
+
+
+// spotify
+// Client ID
+// 6323332f18a543ddb10c6353697c79a4
+// Client Secret
+// f3af806fd33d46b58d8f60ef4713591f
+
+var spotifyKeys = {
+    id: '6323332f18a543ddb10c6353697c79a4',
+    secret: 'f3af806fd33d46b58d8f60ef4713591f',
+  }
+  
+  module.exports = spotifyKeys;
